8e1a62baf67e979f0a65b9a0ea62d08176f89347
Luiz Francisco <luizfk.gmail.com>
2024-05-09 06:47:10 -0300

    Alterando classe, layout refeito e responsivo para smartphones porém sem conseguir adicionar classe de ativação no elemento diretor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
-            const diretor = document.querySelector(`.cast-scenes__director`)
-            const movies = document.querySelector(`.cast-scenes__movies`);
             escondeAbas();
             aba.classList.add('cast-scenes__content--is-active')
-            diretor.classList.add('cast-scenes__director--is-active')
-            movies.classList.add('cast-scenes__movies--is-active')
-            
             removeBotaoAtivo();
             botao.target.classList.add('cast-scenes__button--is-active');
+            
             
         })
     }
 })
-
-
 
 function toggleMenu() {
     const navbar = document.querySelector('.navbar');
@@ -38,9 +33,6 @@ function escondeAbas(){
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
     for (let i = 0; i < tabsContainer.length; i++) {
-        
-        tabsContainer[i].classList.remove('cast-scenes__director--is-active');
-        tabsContainer[i].classList.remove('cast-scenes__movies--is-active');
         tabsContainer[i].classList.remove('cast-scenes__content--is-active');
     }
 }
